0c6e13fe46e7c92b9c43e5d31a9f1737e2b56bfb
provefar <provefars@gmail.com>
2017-12-30 14:10:27 +0800

    获取到蛇的轮廓已经好了

--- main.py
+++ main.py
@@ -5,31 +5,37 @@ import time
 import numpy as np
 from PIL import ImageGrab
 
-mog = cv2.createBackgroundSubtractorMOG2(history=1)
 
 while cv2.waitKey(1) == -1:
     # s,im = camera.read()
     t = time.time()
-    im = ImageGrab.grab((0, 0, 304, 220))
+    im = ImageGrab.grab((0, 0, 740, 492))
     im = np.asarray(im)
 
+
+    # 226 222 216 213
     img = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+    img = np.where((img < 210) | (img > 240), img, 0)
+    img = np.where(img == 0 , img, 255)
 
-    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
-    closed = cv2.morphologyEx(img, cv2.MORPH_CLOSE, kernel)
-
-    closed = cv2.erode(closed, None, iterations=3)
+    closed = cv2.dilate(img, None, iterations=1)
+    closed = cv2.erode(closed, None, iterations=1)
 
 
-    (_, closed) = cv2.threshold(closed, 150, 255, cv2.THRESH_BINARY)
-    closed = cv2.dilate(closed, None, iterations=1)
-    image, contours, hier = cv2.findContours(closed, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    # (_, closed) = cv2.threshold(closed, 200, 1, cv2.THRESH_BINARY)
+
+    image, contours, hier = cv2.findContours(closed, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    cv2.drawContours(closed,contours,-1,128,3)
     for c in contours:
         x, y, w, h = cv2.boundingRect(c)
         area = w * h
-        cv2.rectangle(im, (x, y), (x + w, y + h), (255, 255, 0), 2)
+        if w!=740:
+
+            cv2.rectangle(closed, (x, y), (x + w, y + h), 128 , 2)
     print(time.time() - t)
-    cv2.imshow("new", im)
+    cv2.imshow("img", img)
+
     cv2.imshow("closed", closed)
 
 # camera.release()
