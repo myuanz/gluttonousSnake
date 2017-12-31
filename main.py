a157ea65579dbd816e9059840ed2dc90154618fa
provefar <provefars@gmail.com>
2017-12-31 16:37:25 +0800

    这是无尽模式的备份,似乎赏金模式更容易一些

--- main.py
+++ main.py
@@ -4,14 +4,15 @@ import cv2
 import time
 import numpy as np
 from PIL import ImageGrab
-
-
+from pykeyboard import PyKeyboard
+长 = 945
+高 = 551
+pkd = PyKeyboard()
 while cv2.waitKey(1) == -1:
-    # s,im = camera.read()
-    t = time.time()
-    im = ImageGrab.grab((0, 0, 740, 492))
-    im = np.asarray(im)
 
+    t = time.time()
+    im = ImageGrab.grab((0, 0, 长, 高))
+    im = np.asarray(im)
 
     # 226 222 216 213
     img = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
@@ -19,26 +20,30 @@ while cv2.waitKey(1) == -1:
     img = np.where(img == 0 , img, 255)
 
     closed = cv2.dilate(img, None, iterations=1)
-    closed = cv2.erode(closed, None, iterations=1)
-
-
-    # (_, closed) = cv2.threshold(closed, 200, 1, cv2.THRESH_BINARY)
-
+    #closed = cv2.erode(closed, None, iterations=1)
     image, contours, hier = cv2.findContours(closed, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
     cv2.drawContours(closed,contours,-1,128,3)
     for c in contours:
         x, y, w, h = cv2.boundingRect(c)
         area = w * h
-        if w!=740:
 
-            cv2.rectangle(closed, (x, y), (x + w, y + h), 128 , 2)
+        if area <= -100:
+            print(area)
+            if x<长/2:
+                pkd.tap_key('a')
+            else:
+                pkd.tap_key('d')
+            if y<高/2:
+                pkd.tap_key('w')
+            else:
+                pkd.tap_key('s')
+            break
+            #cv2.rectangle(closed, (x, y), (x + w, y + h), 128 , 2)
     print(time.time() - t)
     cv2.imshow("img", img)
 
     cv2.imshow("closed", closed)
-
-# camera.release()
 
 
 cv2.destroyAllWindows()
